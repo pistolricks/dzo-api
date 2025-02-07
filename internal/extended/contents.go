6b753cf5b2acc0a303459448be11ebe9ae68c4a4
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-02-07 02:33:06 -0800

    added

--- internal/extended/contents.go
+++ internal/extended/contents.go
@@ -1,8 +1,10 @@
 package extended
 
 import (
+	"context"
 	"database/sql"
 	"encoding/hex"
+	"errors"
 	"github.com/devedge/imagehash"
 	"github.com/indrasaputra/hashids"
 	"github.com/pistolricks/validation"
@@ -13,6 +15,7 @@ type Content struct {
 	ID        hashids.ID `json:"id"`
 	CreatedAt time.Time  `json:"-"`
 	Name      string     `json:"name,omitempty"`
+	Original  string     `json:"original,omitempty"`
 	Hash      string     `json:"hash,omitempty"`
 	Src       string     `json:"src"`
 	Type      string     `json:"type,omitempty"`
@@ -35,6 +38,39 @@ func HashImage(image string) string {
 
 type ContentModel struct {
 	DB *sql.DB
+}
+
+func (m ContentModel) GetByHash(hash string) (*Content, error) {
+	query := `
+	SELECT id,created_at,name,original,hash,src,type,size,user_id
+FROM contents
+WHERE hash = $1`
+	var content Content
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, hash).Scan(
+		&content.ID,
+		&content.CreatedAt,
+		&content.Name,
+		&content.Original,
+		&content.Hash,
+		&content.Src,
+		&content.Type,
+		&content.Size,
+		&content.UserID,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &content, nil
 }
 
 func (m ContentModel) EncodeWebP(content *Content) error {
